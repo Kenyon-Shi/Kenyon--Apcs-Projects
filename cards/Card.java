27842f9f4ca5c2caa66f78279ca80df54424d7d3
Kenyon-Shi <kenyonshi@gmail.com>
2025-11-17 22:51:58 +0000

    i finished writing the card class

--- cards/Card.java
+++ cards/Card.java
@@ -4,6 +4,18 @@ public class Card {
 private int suit;
 private int value;
 
-private static String[] SUITS = {}
+private static String[] SUITS = {"♦", "♣", "♥", "♠"};
+private static String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+public Card(int suit, int value)
+{
+    this.suit = suit;
+    this.value = value;
+}
+
+public int getValue()
+{
+    return value;
+}
 
 }
