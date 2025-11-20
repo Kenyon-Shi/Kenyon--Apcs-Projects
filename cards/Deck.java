42bf72884deb3399c62f02a03223a5fd3f76c5ba
Kenyon-Shi <kenyonshi@gmail.com>
2025-11-20 03:20:04 +0000

    i finished writing the deck class

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,77 @@
-package cards;
+import java.util.Random;
 
 public class Deck {
 
+    private Card[] cards;
+    private int topIndex; // next card to draw
 
+    public Deck() {
+        cards = new Card[52];
+        resetDeck();
+    }
+
+    // helper to rebuild the deck (used for bonus fix)
+    private void resetDeck() {
+        int index = 0;
+        for (int suit = 0; suit < 4; suit++) {
+            for (int value = 0; value < 13; value++) {
+                cards[index] = new Card(suit, value);
+                index++;
+            }
+        }
+        topIndex = 0;
+    }
+
+    public void shuffle() {
+        Random rand = new Random();
+        for (int i = 0; i < cards.length; i++) {
+            int swap = rand.nextInt(52);
+            Card temp = cards[i];
+            cards[i] = cards[swap];
+            cards[swap] = temp;
+        }
+        topIndex = 0;
+    }
+
+    public void cut(int n) {
+        if (n < 0) n = 0;
+        if (n > 51) n = 51;
+
+        Card[] newDeck = new Card[52];
+        int pos = 0;
+
+        for (int i = n; i < 52; i++) {
+            newDeck[pos++] = cards[i];
+        }
+        for (int i = 0; i < n; i++) {
+            newDeck[pos++] = cards[i];
+        }
+
+        cards = newDeck;
+        topIndex = 0;
+    }
+
+    public Card draw() {
+        // BONUS FIX — prevents index out of bounds
+        if (topIndex >= 52) {
+            resetDeck();
+        }
+
+        Card c = cards[topIndex];
+        topIndex++;
+        return c;
+    }
+
+    public void print(int num) {
+        StringBuilder sb = new StringBuilder();
+        int end = topIndex + num;
+        if (end > 52) end = 52;
+
+        for (int i = topIndex; i < end; i++) {
+            sb.append(cards[i].toString());
+            if (i < end - 1) sb.append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
--- cards/Hand.java
+++ cards/Hand.java
@@ -1,5 +1,45 @@
-package cards;
-
 public class Hand {
 
+    private Card[] cards;
+    private int size;
+
+    public Hand(int max) {
+        cards = new Card[max];
+        size = 0;
+    }
+
+    public void add(Card c) {
+        if (size < cards.length) {
+            cards[size] = c;
+            size++;
+        }
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public Card get(int index) {
+        return cards[index];
+    }
+
+    public Card remove(int index) {
+        Card removed = cards[index];
+
+        for (int i = index; i < size - 1; i++) {
+            cards[i] = cards[i + 1];
+        }
+
+        size--;
+        return removed;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(cards[i].toString());
+            if (i < size - 1) sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
