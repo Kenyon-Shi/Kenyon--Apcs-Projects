d331c0f12fe88dfb784b08efbf00362d5af4acbc
Kenyon-Shi <kenyonshi@gmail.com>
2026-01-09 23:01:30 +0000

    i finished the 2nd problem

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,9 +1,19 @@
 package warmup;
 
 public class Main {
-
     public static void main(String[] args) {
-        // Your code here.
 
+        int limit = 1000;      // numbers below this
+        int sum = 0;
+
+        for (int i = 1; i < limit; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+
+        }
+
+        System.out.println("Sum = " + sum);
     }
 }
