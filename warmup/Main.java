d64e021d61a9881e6122e9121460bde3fcf86c53
Kenyon-Shi <kenyonshi@gmail.com>
2026-01-26 23:10:24 +0000

    jjj

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,15 +3,22 @@ package warmup;
 public class Main {
     public static void main(String[] args) {
 
-        int limit = 1000;      // numbers below this
-        int sum = 0;
+        int limit = 4_000_000;
 
-        for (int i = 1; i < limit; i++) {
+        int a = 1;     // first Fibonacci term
+        int b = 2;     // second Fibonacci term
 
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
+        long sum = 0;  
+
+        while (a <= limit) {
+
+            if (a % 2 == 0) {
+                sum += a;
             }
 
+            int next = a + b;
+            a = b;
+            b = next;
         }
 
         System.out.println("Sum = " + sum);
