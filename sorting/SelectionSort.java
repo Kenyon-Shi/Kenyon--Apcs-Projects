8f62304fbb23c6440963e60e6d08fea80b2449f0
Kenyon-Shi <kenyonshi@gmail.com>
2026-01-29 22:20:00 +0000

    Im trying to complete the Selection Sort File

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,7 +2,31 @@ package sorting;
 
 public class SelectionSort implements Sorter {
 
+    private long steps = 0;
+
+    @Override
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+
+        // Selection Sort implementation
+        steps = 0;
+
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < input.length; j++) {
+                steps++; // comparison
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = input[i]; steps++;
+                input[i] = input[minIndex]; steps++;
+                input[minIndex] = temp; steps++;
+            }
+        }
+
+        System.out.println("Selection Sort steps: " + steps);
     }
 }
