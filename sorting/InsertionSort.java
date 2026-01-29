678996cda1683e714c4611ed0862fb6cbd107a46
Kenyon-Shi <kenyonshi@gmail.com>
2026-01-29 22:21:02 +0000

    im working on insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -2,7 +2,31 @@ package sorting;
 
 public class InsertionSort implements Sorter {
 
+    private long steps = 0;
+
+    @Override
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+
+        // Insertion Sort implementation
+        steps = 0;
+
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i]; steps++;
+            int j = i - 1;
+
+            while (j >= 0) {
+                steps++; // comparison
+                if (input[j] > key) {
+                    input[j + 1] = input[j]; steps++;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            input[j + 1] = key; steps++;
+        }
+
+        System.out.println("Insertion Sort steps: " + steps);
     }
 }
